f7c2998faf71c46808dfd0e5f255da440243e8a6
Miho <zmyhail@gmail.com>
2021-04-07 22:52:15 +0300

    Add PostreSQL

--- events/models.py
+++ events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+	"""docstring for Event"""
+	#def __init__(self, arg):
+		#super(Event, self).__init__()
+		#self.arg = arg
+	event_image = models.ImageField(upload_to='event_images/')
+	event_text = models.CharField(max_length=300)
+		
