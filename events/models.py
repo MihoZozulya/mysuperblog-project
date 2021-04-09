0e41dd71f399c0fd3b2c49812db676aca7097816
Miho <zmyhail@gmail.com>
2021-04-09 21:04:29 +0300

    Add static

--- events/models.py
+++ events/models.py
@@ -8,4 +8,7 @@ class Event(models.Model):
 		#self.arg = arg
 	event_image = models.ImageField(upload_to='event_images/')
 	event_text = models.CharField(max_length=300)
-		
+
+
+	def __str__(self):
+		return self.event_text		
